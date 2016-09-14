e9c89e9e2e6471800eb1f7d0d60df6ffaf91aac3
laaptu <laaptu@laaptu.local>
2016-09-14 14:01:06 +0545

    Making a new repo for DI demo

---
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/fragments/FormDisplayFragment.java
@@ -0,0 +1,10 @@
+package com.wordpress.laaptu.dependencyinjection.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by laaptu on 9/14/16.
+ */
+
+public class FormDisplayFragment extends Fragment {
+}
---
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/fragments/FormEditFragment.java
@@ -0,0 +1,9 @@
+package com.wordpress.laaptu.dependencyinjection.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ */
+
+public class FormEditFragment extends Fragment {
+}

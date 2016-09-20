90c47feece23711f81a06b69c0c7b89338355b53
laaptu <laaptu@laaptu.local>
2016-09-20 15:00:26 +0545

    Completing info display action

--- app/src/main/java/com/wordpress/laaptu/dependencyinjection/MainActivity.java
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/MainActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import com.wordpress.laaptu.dependencyinjection.fragments.FormDisplayFragment;
 import com.wordpress.laaptu.dependencyinjection.fragments.FormEditFragment;
 
 public class MainActivity extends AppCompatActivity {
 
   Toolbar toolbar;
+
+  private static final String FRAG_EDIT = "FormEditFragment", FRAG_DISPLAY = "FormDisplayFragment";
+
+  public enum FragState {
+    Edit, Display;
+  }
+
+  private FragState fragState;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,13 +26,38 @@ public class MainActivity extends AppCompatActivity {
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
-    getSupportFragmentManager().beginTransaction()
-        .replace(R.id.container, FormEditFragment.getInstance(null))
-        .commit();
+    changeFragment(FragState.Display);
+  }
+
+  public void changeFragment(FragState fragState) {
+    if (this.fragState != null && this.fragState == fragState) return;
+    this.fragState = fragState;
+    switch (fragState) {
+      case Edit:
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.container, FormEditFragment.getInstance(null), FRAG_EDIT)
+            .commit();
+        break;
+      case Display:
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.container, FormDisplayFragment.getInstance(null), FRAG_DISPLAY)
+            .commit();
+        break;
+    }
+    invalidateOptionsMenu();
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.main, menu);
+    if (this.fragState != null) {
+      if (fragState == FragState.Edit) {
+        menu.findItem(R.id.menu_edit).setVisible(false);
+        menu.findItem(R.id.menu_save).setVisible(true);
+      } else {
+        menu.findItem(R.id.menu_edit).setVisible(true);
+        menu.findItem(R.id.menu_save).setVisible(false);
+      }
+    }
     return super.onCreateOptionsMenu(menu);
   }
 }
--- app/src/main/java/com/wordpress/laaptu/dependencyinjection/fragments/FormDisplayFragment.java
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/fragments/FormDisplayFragment.java
@@ -3,20 +3,25 @@ package com.wordpress.laaptu.dependencyinjection.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.TextAppearanceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import com.wordpress.laaptu.dependencyinjection.R;
+import com.wordpress.laaptu.dependencyinjection.model.User;
 
 /**
  */
 
 public class FormDisplayFragment extends Fragment {
 
-
-
-  private EditText infoText;
+  private TextView infoText;
+  private int titleStyle = R.style.DisplayTitleTextStyle, infoStyle = R.style.DisplayInfoTextStyle;
 
   public FormDisplayFragment() {
 
@@ -31,11 +36,34 @@ public class FormDisplayFragment extends Fragment {
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.form_display, container, false);
+    View view = inflater.inflate(R.layout.form_display, container, false);
+    infoText = (TextView) view.findViewById(R.id.info_txt);
+    return view;
   }
 
   @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
-    
+    setUserInfo(User.getUser());
+  }
+
+  private void setUserInfo(User user) {
+    if (user == null) return;
+    SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+    appendValues(spannableStringBuilder, getString(R.string.name), user.name);
+    appendValues(spannableStringBuilder, getString(R.string.email), user.email);
+    appendValues(spannableStringBuilder, getString(R.string.address), user.address);
+    appendValues(spannableStringBuilder, getString(R.string.phone), user.phone);
+    infoText.setText(spannableStringBuilder);
+  }
+
+  private void appendValues(SpannableStringBuilder ssb, String name, String value) {
+    int startIndex = ssb.length();
+    ssb.append(name.concat(":\n"));
+    ssb.setSpan(new TextAppearanceSpan(getContext(), R.style.DisplayTitleTextStyle), startIndex,
+        ssb.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+    startIndex = ssb.length();
+    ssb.append(value.concat("\n\n"));
+    ssb.setSpan(new TextAppearanceSpan(getContext(), R.style.DisplayInfoTextStyle), startIndex,
+        ssb.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
   }
 }

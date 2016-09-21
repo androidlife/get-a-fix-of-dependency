9222c516bc16ec73e33eb05be1f62356986b6d5e
laaptu <laaptu@laaptu.local>
2016-09-21 13:02:18 +0545

    Writing test for edit and save

--- app/src/main/java/com/wordpress/laaptu/dependencyinjection/widgets/EditFormTextInputLayout.java
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/widgets/EditFormTextInputLayout.java
@@ -2,7 +2,13 @@ package com.wordpress.laaptu.dependencyinjection.widgets;
 
 import android.content.Context;
 import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import com.wordpress.laaptu.dependencyinjection.R;
 
 /**
@@ -19,5 +25,28 @@ public class EditFormTextInputLayout extends TextInputLayout {
 
   public EditFormTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, R.attr.textInputLayoutStyle);
+  }
+
+  @Override public void addView(View child, int index, ViewGroup.LayoutParams params) {
+    super.addView(child, index, params);
+    EditText editText = getEditText();
+    if (editText != null) {
+      editText.addTextChangedListener(new TextWatcher() {
+        @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override public void afterTextChanged(Editable editable) {
+          if (!TextUtils.isEmpty(editable.toString()) && getError() != null) {
+            setError(null);
+            setErrorEnabled(false);
+          }
+        }
+      });
+    }
   }
 }

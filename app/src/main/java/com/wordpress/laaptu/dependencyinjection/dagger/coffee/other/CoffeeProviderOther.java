a37c409d9bf1d8abd56514979135349c241511ab
laaptu <sanimap@gmail.com>
2017-05-16 14:02:15 +0545

    Some minor changes

--- app/src/main/java/com/wordpress/laaptu/dependencyinjection/dagger/coffee/other/CoffeeProviderOther.java
+++ app/src/main/java/com/wordpress/laaptu/dependencyinjection/dagger/coffee/other/CoffeeProviderOther.java
@@ -18,6 +18,8 @@ public class CoffeeProviderOther {
         return 10;
     }
 
+    //if this provide is included, it will throw compile error as
+    //dagger is not clear which quantity to use
 //    @Provides int values(){
 //        return 20;
 //    }
@@ -31,6 +33,11 @@ public class CoffeeProviderOther {
     public CoffeeHelper coffeeHelper(int quantity, Coffee.Flavor flavor) {
         return new CoffeeHelper(quantity, flavor);
     }
+
+//    @Provides
+//    public CoffeeHelper coffeeHelper() {
+//        return new CoffeeHelper(10, Coffee.Flavor.Espresso);
+//    }
 
 
 }
